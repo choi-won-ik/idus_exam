abf6aa5fb01b2154d6ec0ed87cc50f6fae07dad4
choi-won-ik <whwwhs7837@gmail.com>
2025-02-11 10:47:02 +0900

    start

--- src/main/java/com/example/day0211/config/security/SecurityConfig.java
+++ src/main/java/com/example/day0211/config/security/SecurityConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
     private final AuthenticationConfiguration configuration;
 
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
